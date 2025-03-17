790feef0676b88616eac0fe133870a74857209ee
Zan Naqvi <naqvizh@uci.edu>
2025-03-17 00:11:07 +0000

    in dj.cpp i made a lot of syntax errors, and I haven't checked any errors beyond compiling errors

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -3,27 +3,26 @@
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous)
 {
     int size = G.size();
-    if (num == 0 || source < 0 || source >= size) return vector<int>(); // initial checks
+    if (size == 0 || source < 0 || source >= size) return {}; // initial checks
     
     vector<int> distances(size, INF);
     distances[source] = 0;
     previous[source] = -1;
     priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-    pq.push((source, 0)); // wait is it source 0 or 0 source
+    pq.push({0, source});
 
     while (!pq.empty()) {
-        auto [dist, i] = pq.top();
+        auto [dist, eu] = pq.top();
         pq.pop();
 
-        if (dist > distances[i]) {
-            for (const Edge& edge : G(i)) {
-                int ev = edge.dst; // destination vector
-                int eweight = edge.weight; // the weight
-                if (distances[i] + weight < distances[ev]) {
-                    distances[ev] = distances[i] + weight;
-                    previous[ev] = i;
-                    pq.push({distances[i], ev});
-                }
+        if (dist > distances[eu]) continue; // ignores outdated entries
+        for (const Edge& edge : G[eu]) { // ADJ LIST
+            int ev = edge.dst; // destination vector
+            int weight = edge.weight; // the weight
+            if (distances[eu] + weight < distances[ev]) {
+                distances[ev] = distances[eu] + weight;
+                previous[ev] = eu;
+                pq.push({distances[ev], ev});
             }
         }
     }
@@ -33,7 +32,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
 vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination)
 {
     vector<int> path;
-    if (previous[destination] == INF || previous[destination] == -1) return path; // unreachable?
+    if (previous[destination] == INF || previous[destination] == -1) return path; // unreachables
     for (int i = destination; i != -1; i = previous[i]) path.push_back(i); // add it backwards
     reverse(path.begin(), path.end()); // fix it
     return path;
@@ -46,3 +45,5 @@ void print_path(const vector<int>& v, int total)
     }
     cout << "\nTotal cost is " << total << "\n";
 } // DONE
+
+// g++ -o dj_main src/dijkstras.cpp src/dijkstras_main.cpp
---
+++ src/dijkstras_main.cpp
@@ -0,0 +1,7 @@
+#include "dijkstras.h"
+
+int main()
+{
+    cout << ":3" << endl;
+    return 0;
+}
