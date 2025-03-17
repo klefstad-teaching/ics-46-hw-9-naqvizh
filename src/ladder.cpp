da38840982f8e13e57d869fc28934176e935b1d2
Zan Naqvi <naqvizh@uci.edu>
2025-03-17 01:35:43 +0000

    Implemented every function in ladder.cpp

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -96,12 +96,21 @@ vector<string> generate_word_ladder(const string& begin_word, const string& end_
         }   
     }
     return {};
-}
+} // DONE?
 
 void load_words(set<string> & word_list, const string& file_name)
 {
-    return;
-}
+    ifstream file(file_name);
+    if (!file) {
+        cerr << "Error: Cannot open file: " << file_name << endl;
+        return;
+    }
+    string word;
+    while (file >> word) {
+        word_list.insert(word);
+    }
+    file.close();
+} // DONE?
 
 void print_word_ladder(const vector<string>& ladder)
 {
@@ -110,14 +119,24 @@ void print_word_ladder(const vector<string>& ladder)
         return;
     }
     for (const string& word : ladder) {
-        cout << word << " " << endl;
+        cout << word << " ";
     }
     cout << endl;
 } // DONE?
 
-void verify_word_ladder()
-{
-    return;
-}
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
+
+void verify_word_ladder() {
+    set<string> word_list;
+
+    load_words(word_list, "words.txt");
+
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+} // DONE?
 
 // g++ -o ladder_main src/ladder.cpp src/ladder_main.cpp
